f524a9929bddf60d9e60f6a4e41b1bbd2151a6c5
Cgsmith24 <bubbaglenn22@gmail.com>
2022-10-01 11:30:45 -0600

    Update code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Drivetrain.java
@@ -63,9 +63,9 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Omni Linear OpMode", group="Linear Opmode")
+@TeleOp(name="Drivetrain Opmode", group="Linear Opmode")
 //@Disabled
-public class BasicOmniOpMode_Linear extends LinearOpMode {
+public class Drivetrain extends LinearOpMode {
 
     // Declare OpMode members for each of the 4 motors.
     private ElapsedTime runtime = new ElapsedTime();
